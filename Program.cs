8e995856a8576676bcc9d9845634461fb5bcaa24
Juan Blanco <juanfranblanco@hotmail.com>
2025-06-04 18:50:43 +0100

    Adding signing, function pre loading abi encoding

--- Program.cs
+++ Program.cs
@@ -4,6 +4,13 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using Nethereum.GnosisValidator;
 using Serilog;
+using Nethereum.Blazor;
+using Nethereum.EIP6963WalletInterop;
+using Nethereum.Blazor.Storage;
+using Nethereum.UI;
+using Microsoft.AspNetCore.Components.Authorization;
+using Nethereum.Blazor.EIP6963WalletInterop;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -16,6 +23,22 @@ if (!builder.RootComponents.Any())
 
 builder.Services.AddMudServices();
 
+builder.Services.AddAuthorizationCore();
+builder.Services.AddSingleton<IEIP6963WalletInterop, EIP6963WalletBlazorInterop>();
+builder.Services.AddSingleton<EIP6963WalletHostProvider>();
+builder.Services.AddSingleton<LocalStorageHelper>();
+
+//Add eip as the selected ethereum host provider
+builder.Services.AddSingleton(services =>
+{
+    var walletHostProvider = services.GetService<EIP6963WalletHostProvider>();
+    var selectedHostProvider = new SelectedEthereumHostProviderService();
+    selectedHostProvider.SetSelectedEthereumHostProvider(walletHostProvider);
+    return selectedHostProvider;
+});
+
+
+builder.Services.AddSingleton<AuthenticationStateProvider, EthereumAuthenticationStateProvider>();
 
 
 Log.Logger = new LoggerConfiguration()
@@ -23,5 +46,7 @@ Log.Logger = new LoggerConfiguration()
     .WriteTo.BrowserConsole()
     .CreateLogger();
 
-await builder.Build().RunAsync();
+var app = builder.Build();
+    
+await app.RunAsync();
 
